12d89648d78e961e6feeed730e04b93a69119832
kkallen <168973391+kkallen1@users.noreply.github.com>
2025-12-05 15:04:18 +0800

    Delete 110_T directory

--- 110_T/TEST-10921.py
+++
@@ -1,3 +0,0 @@
-for _ in range(int(input())):
-    data = list(int(x) for x in input().split())
-    print(max(data))
--- 110_T/TEST-10932.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-for _ in range(int(input())):
-    n = int(input())
-
-    date = datetime.date(1970, 1, 1)
-    date += datetime.timedelta(days=n)
-    print(date)
